99fdf593424e2da6e13cac79249f6a2d94f2ad2e
ferdaus <ferdaushossan7788990@gmail.com>
2025-01-14 10:46:22 +0600

    showDailog box

--- lib/future/dailogbox/dialog_box_screen.dart
+++ lib/future/dailogbox/dialog_box_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
 class DialogBoxScreen extends StatefulWidget {
   const DialogBoxScreen({super.key});
 
@@ -11,8 +13,40 @@ class _DialogBoxScreenState extends State<DialogBoxScreen> {
   Widget build(BuildContext context) {
     return  Scaffold(
       backgroundColor: Colors.white,
-
+        body: Center(
+          child: CircleAvatar(child: IconButton(onPressed: (){
+            Box();
+          }, icon: Icon(Icons.face,color: Colors.red,size: 61,),),radius: 50,),
+        ),
 
     );
   }
-}
+
+  void Box() {
+    showDialog(context: context, builder: (BuildContext context){
+     return AlertDialog(
+     title: Text('ferdaus'),
+        backgroundColor: Colors.white,
+        elevation: 4,
+        content: Text('hi my name is ferdaus hossan what is your name & what is your job in this company '),
+       actions: [
+         Row(
+           mainAxisAlignment: MainAxisAlignment.spaceBetween,
+           children: [
+             TextButton(onPressed: (){
+               Get.back();
+             },child: Text('OK'),),
+             TextButton(onPressed: (){},child: Text('NO'),),
+           ],
+         ),
+
+
+       ],
+      );
+    });
+  }
+  }
+
+
+
+
